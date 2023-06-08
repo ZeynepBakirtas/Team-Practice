b3f6d8678506bd9c897e38073a05ac35257ad2d8
ZeynepBakirtas <zeyneppbakirtas@gmail.com>
2023-06-08 22:39:44 +0200

    v

--- src/main/java/task/PerfectNumber.java
+++ src/main/java/task/PerfectNumber.java
@@ -1,5 +1,9 @@
 package task;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class PerfectNumber {
     public static void main(String[] args) {
         /* Perfect Number (Mukemmel sayi)
@@ -9,10 +13,36 @@ public class PerfectNumber {
       kendisine esitse o sayi mukemmeldir. (1+2+3 = 6 = 6 (Mukemmel))
       ORNEK: (6, 28, 496, 8128)
       INPUT      : 6
-      OUTPUT     :  1,2,3
+      OUTPUT     : 1,2,3
     */
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayi giriniz");
+        int number = input.nextInt();
 
+        mukemmelMi(number);
+
+    }
+
+    private static void mukemmelMi(int number) {
+
+
+        List<Integer> bolenList = new ArrayList<>();
+
+        int bolenToplam = 0;
+
+        for (int i = 1; i < number; i++) {//bolumunden kalan 0 olursa calisacak
+            if(number % i == 0){
+                bolenList.add(i);
+                bolenToplam += i;
+            }
+        }
+
+        if (bolenToplam == number){
+            System.out.println(number+ " Sayisi mukemmeldir \n"+ "Bolenler listesi: " + bolenList);
+        } else {
+            System.out.println(number + " Sayiniz mukemmel degildir.");
+        }
 
     }
 }
